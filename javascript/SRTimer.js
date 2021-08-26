7af0797c9b50e2b3a4b983345caf18a390353d51
dsszymanski <dan1993us@yahoo.com>
2021-08-26 17:53:57 -0400

    Added pause button functionality

--- javascript/SRTimer.js
+++ javascript/SRTimer.js
@@ -1,5 +1,7 @@
 //global variable that stores the time the timer is clicked in ms.
 var startTime;
+
+var storedTime = 0;
 //interval variable for main timer
 var timerInterval;
 //const used for interval increments
@@ -26,6 +28,36 @@ let stopClick = () => {
 	btn.innerText = 'Start';
 	btn.onclick = startClick;
 	clearInterval(timerInterval);
+	storedTime = 0;
+}
+
+let pauseClick = () => {
+	storedTime = (Date.now() - startTime) + storedTime;
+	clearInterval(timerInterval);
+
+	let mainBtn = document.getElementById('mainBtn');
+	mainBtn.innerText = 'Continue';
+	mainBtn.onclick = continueClick;
+
+	let pauseBtn = document.getElementById('pauseBtn');
+	pauseBtn.innerText = 'Reset';
+	pauseBtn.onclick = resetClick;
+}
+
+let continueClick = () => {
+	let pauseBtn = document.getElementById('pauseBtn');
+	pauseBtn.innerText = 'Pause';
+	pauseBtn.onclick = pauseClick;
+
+	startClick();
+}
+
+let resetClick = () => {
+	storedTime = 0;
+
+	let pauseBtn = document.getElementById('pauseBtn');
+	pauseBtn.innerText = 'Pause';
+	pauseBtn.onclick = pauseClick;
 }
 
 let startTimer = () => {
@@ -33,7 +65,7 @@ let startTimer = () => {
 		let hours = document.getElementById('hrs');
 		let mins = document.getElementById('mins');
 		let secs = document.getElementById('secs');
-		let timeElapsed = (Date.now() - startTime)*10;
+		let timeElapsed = (Date.now() - startTime) + storedTime;
 
 		let hr = Math.floor(timeElapsed / toHrs);
 		timeElapsed -= hr * toHrs;
